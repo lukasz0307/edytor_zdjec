316202a42f119dd456ec334d97257591b365fcda
PatrykD2pr <patrykdomaszek@gazeta.pl>
2022-10-18 15:12:54 +0200

    przezroczystosc zrobiona

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,15 +4,62 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+float transparency;
+float rotateX;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar_transparency=(SeekBar) findViewById(R.id.transparency);
+        SeekBar seekBar_rotate=(SeekBar) findViewById(R.id.rotate);
+        ImageView imageview=(ImageView) findViewById(R.id.image);
+        seekBar_transparency.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+           transparency= (float) (progress*0.1);
+           imageview.setAlpha(transparency);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    });
+
+        seekBar_rotate.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+           rotateX= (float) (progress*0.1);
+           imageview.setRotationY(rotateX);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    });
     }
     public void click(View view){
+        TextView text=(TextView) findViewById(R.id.text);
+
+
 
     }
 }
